4a2d134a61bf1575492fec85cdb91891ecf7b214
Jordy van Kuijk <info@noproblem.software>
2022-05-04 09:26:52 +0200

    Initial Commit

---
+++ Shared/DemoZoomScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoZoomScrollViewApp.swift
+//  Shared
+//
+//  Created by Jordy van Kuijk on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoZoomScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
